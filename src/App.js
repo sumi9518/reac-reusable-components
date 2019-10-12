0e7125a520f7d73facd7d9a0c4f1c997514680ac
sumanyadav9518@gmail.com <sumanyadav9518@gmail.com>
2019-10-12 12:31:08 +0200

    StateFull & StateLess communication

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@ import React from 'react';
 import Alert from "./ui-component/Alert";
 
 class App extends React.Component {
+    constructor(){
+        super();
+        this.state ={
+            showAlert: false
+        }
+    }
     render() {
         return (
             <div className="container">
                 <Alert
                     type="danger"
                     message="Its danger Alert"
+                    show = {this.state.showAlert}
                 />
+
                 <Alert
                     type="success"
                     message="Its success Alert"
+
                 />
-                <Alert type="warning">
+
+                <Alert type="warning" >
                     <h2 className="alert alert-header"> Alert Header without msg </h2>
                 </Alert>
-                <Alert type="primary" message="Its primary Alert with header in children">
+
+                <Alert type="primary" message="Its primary Alert with header in children"
+                >
                     <h2 className="alert alert-header"> Alert Header </h2>
                 </Alert>
             </div>
--- src/ui-component/Alert.jsx
+++ src/ui-component/Alert.jsx
@@ -3,17 +3,25 @@ import  propTypes from 'prop-types';
 
 const Alert = (props) => {
     return (
-        <div className={`alert alert-${props.type}`} role="alert">
-           {props.message ? props.message : props.children}
+        <div>
+            {
+                props.show &&
+                <div className={`alert alert-${props.type}`} role="alert">
+                    {props.message ? props.message : props.children}
+                </div>
+            }
         </div>
     );
 };
 Alert.propTypes = {
     type: propTypes.string.isRequired,
-    message: propTypes.string
+    message: propTypes.string,
+    show: propTypes.bool
 }
 Alert.defaultProps={
-    message: "deafult message"
+    type:"primary",
+    message: "deafult message",
+    show: true
 }
 
 export default Alert;
